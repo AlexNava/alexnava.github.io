070216807160ee6e46487ab93787d003575f2a73
Anava <Alex.il.nava@gmail.com>
2014-03-28 01:10:30 +0100

    Stuff added, preparing to use buffer objects

--- GlStuff.js
+++ GlStuff.js
@@ -3,12 +3,67 @@ var gl;
 
 function webGLStart() {
     var canvas = document.getElementById("WebGL-test0");
-    //initGL(canvas);
+    initGL(canvas);
+    
     //initShaders();
     //initBuffers();
 
     gl.clearColor(0.5, 0.5, 0.5, 1.0);
     gl.enable(gl.DEPTH_TEST);
 
+    // Start looping
+    tick();
+}
+
+function initGL(canvas) {
+    try {
+        gl = canvas.getContext("experimental-webgl");
+        gl.viewportWidth = canvas.width;
+        gl.viewportHeight = canvas.height;
+    } catch (e) {
+    }
+    if (!gl) {
+        alert("Could not initialise WebGL, sorry :-(");
+    }
+}
+
+function drawScene() {
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
+
+    mat4.identity(mvMatrix);
+
+    mat4.translate(mvMatrix, [0.0, 0.0, -7.0]);
+
+    mat4.rotate(mvMatrix, degToRad(angle), [0, 0, 1]);
+    // draw object
+    var vertices = [
+         0.0,  1.0,  0.0,
+        -1.0, -1.0,  0.0,
+         1.0, -1.0,  0.0
+    ];
+
+}
+
+// Global timer
+var lastTime = 0;
+
+function animate() {
+    var timeNow = new Date().getTime();
+    if (lastTime != 0) {
+        var elapsed = timeNow - lastTime;
+        
+        // Update stuff based on timers
+        angle += elapsed * 60.0 * 0.001;
+    }
+    lastTime = timeNow;
+}
+
+
+function tick() {
+    requestAnimFrame(tick);
     drawScene();
-  }
+    animate();
+}
